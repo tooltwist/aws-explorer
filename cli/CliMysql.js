0e38e488ac4d18a65bf533b06e9404dbe1b4e40a
Philip Callender <philcal@mac.com>
2018-06-07 23:47:46 +0800

    Only create one phpmyadmin instance

--- cli/CliMysql.js
+++ cli/CliMysql.js
@@ -1,5 +1,7 @@
 var program = require('commander');
 const colors = require('colors')
+const myAWS = require('../server/misc/myAWS')
+
 
 function CliMysql(ip1, ip2, dbhost, dbname, username, password) {
   // console.log(`jumpbox ${ip1} ${ip2}`);
@@ -9,11 +11,12 @@ function CliMysql(ip1, ip2, dbhost, dbname, username, password) {
 
   let localPort = Math.floor(Math.random() * 10000) + 30000
   let httpPort = localPort + 10000;
+  let phpmyadminPort = 49494
   // let keyfile = `~/.ssh/nbt-${e}-${r}.pem`
-  let tunnelCmd = `ssh -nNT -i ${keyfile} -L ${localPort}:${ip2}:22 ec2-user@${ip1} -L ${httpPort}:${ip2}:${httpPort} ec2-user@${ip1}      `
+  let tunnelCmd = `ssh -nNT -i ${keyfile} -L ${localPort}:${ip2}:22 ec2-user@${ip1} -L ${httpPort}:${ip2}:${phpmyadminPort} ec2-user@${ip1}      `
   let loginCmd = `ssh -t -i ${keyfile} -p ${localPort} ec2-user@127.0.0.1       `
   let mysqlCmd = `ssh -t -i ${keyfile} -p ${localPort} ec2-user@127.0.0.1 docker run -it --rm mysql mysql -h ${dbhost}  -u ${username} -p${password} ${dbname}       `
-  let phpmyadminCmd = `ssh -t -i ${keyfile} -p ${localPort} ec2-user@127.0.0.1 docker run -it --rm -p ${httpPort}:80 -e PMA_HOST=${dbhost} phpmyadmin/phpmyadmin:4.6.4-1        `
+  let phpmyadminCmd = `ssh -t -i ${keyfile} -p ${localPort} ec2-user@127.0.0.1 docker run -it --rm -p ${phpmyadminPort}:80 -e PMA_HOST=${dbhost} phpmyadmin/phpmyadmin:4.6.4-1        `
   let browserCmd = `open http://localhost:${httpPort}        `
 
   console.log(`
@@ -24,7 +27,7 @@ function CliMysql(ip1, ip2, dbhost, dbname, username, password) {
               The commands below will use an SSH tunnel to the ECS Host via the jumpbox.
 
                       localhost:${localPort} -> jumpbox (${ip1}) -> ${ip2}:22
-                      localhost:${httpPort} -> jumpbox (${ip1}) -> ${ip2}:${httpPort}
+                      localhost:${httpPort} -> jumpbox (${ip1}) -> ${ip2}:${phpmyadminPort}
 
               The first tunnel allows SSH access to the ECS host, while the second tunnel
               allows http access to PhpMyadmin. First, start the SSH tunnel:
